0452f55e03eac1d8d3374cc20309a7d77424134a
slimMrabet <slim.mrabet@gmail.com>
2018-10-22 22:33:32 +0200

    [Account Manager] Mock data layer

--- src/main/java/org/sid/data/AccountRepository.java
+++ src/main/java/org/sid/data/AccountRepository.java
@@ -1,8 +1,12 @@
 package org.sid.data;
 
+import org.sid.entities.Account;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AccountRepository {
-
+	public Account getAccount(String accountId) {
+		// mock the function, simulation of data layer
+		return new Account();
+	}
 }
--- src/main/java/org/sid/data/OperationRepository.java
+++ src/main/java/org/sid/data/OperationRepository.java
@@ -1,8 +1,18 @@
 package org.sid.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sid.entities.Operation;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class OperationRepository {
+	public Operation setOperation(Operation operation) {
+        return operation;
+    }
 
+    public List<Operation> getAll(String accountId) {
+        return new ArrayList<>();
+    }
 }
