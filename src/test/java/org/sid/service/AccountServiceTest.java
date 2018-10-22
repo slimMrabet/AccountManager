9d6c493ede8fa086536ac3896e770fccac9307ba
slimMrabet <slim.mrabet@gmail.com>
2018-10-22 22:35:13 +0200

    [Account Manager] Implement account service test

--- src/test/java/org/sid/service/AccountServiceTest.java
+++ src/test/java/org/sid/service/AccountServiceTest.java
@@ -1,5 +1,36 @@
 package org.sid.service;
 
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.sid.core.exception.InvalidAccountException;
+import org.sid.data.AccountRepository;
+
+import junit.framework.Assert;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AccountServiceTest {
+    @InjectMocks
+    AccountService accountService;
+
+    @Mock
+    AccountRepository accountRepository;
+    @Test(expected = InvalidAccountException.class)
+    public void when_account_not_found_expect_exception() throws InvalidAccountException {
+
+        //given
+        String accountId = "myId";
+        when(accountRepository.getAccount(accountId)).thenReturn(null);
+
+        //when
+        accountService.getAccount(accountId);
+
+        //then
+        Assert.fail("InvalidAccountException not thrown !");
+    }
 
 }
