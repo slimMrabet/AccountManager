cd5fe8fd68a41523a330592531995102ba87d644
slimMrabet <slim.mrabet@gmail.com>
2018-10-22 22:40:31 +0200

    [Account Manager] : implement account service methode

--- src/main/java/org/sid/service/AccountService.java
+++ src/main/java/org/sid/service/AccountService.java
@@ -1,13 +1,22 @@
 package org.sid.service;
 
+import org.sid.core.exception.InvalidAccountException;
+import org.sid.data.AccountRepository;
 import org.sid.entities.Account;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
-	
-	public Account getAccount(String accountId) {
-		
+    @Autowired
+    AccountRepository accountRepository;
+
+	public Account getAccount(String accountId) throws InvalidAccountException {
+		Account account =  accountRepository.getAccount(accountId);
+		if (account == null) {
+			  throw new InvalidAccountException();
+		}
+		return account;
 	}
 
 }
